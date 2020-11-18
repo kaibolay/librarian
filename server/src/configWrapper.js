e50c2dab8790a07deb93637a61e7f37363f73572
Cabpy <Cabbageboy@users.noreply.github.com>
2020-11-18 10:41:56 -0500

    Update configWrapper.js

--- server/src/configWrapper.js
+++ server/src/configWrapper.js
@@ -1,7 +1,5 @@
 const config = require('config');
 
-
-<<<<<<< HEAD
 function ConfigWrapper(inputCfg = config) {
     this.cfg = inputCfg;
 }
@@ -11,17 +9,3 @@ ConfigWrapper.prototype.get = function (key) {
 }
 
 module.exports = ConfigWrapper;
-=======
-var configWrapper = function () {
-    // if it's using config
-    return config;
-}
-
-// configWrapper.prototype.get = function (key) {
-//     return config.get(key);
-// };
-
-module.exports = {
-    configWrapper: new configWrapper()
-};
->>>>>>> f59b4b9816bc07c60cd409bc55f2608f8a6be05f
